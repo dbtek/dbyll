04c2a170117c53b1dd664b016fad0bf2b113d47e
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-02-11 16:59:23 +0200

    categories & tags tabs are being managed through hash value. initial tab activation based on hash added. fixes #31.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,41 @@
 (function($) {
-	$(document).ready(function() {
 
-		/* Sidebar height set */
-		$('.sidebar').css('min-height',$(document).height());
+ function init() {
+    /* Sidebar height set */
+    $('.sidebar').css('min-height',$(document).height());
 
-		/* Secondary contact links */
-		var $scontacts = $('#contact-list-secondary');
-		var $contactList = $('#contact-list');
+    /* Secondary contact links */
+    var $scontacts = $('#contact-list-secondary');
+    var $contactList = $('#contact-list');
 
-		$scontacts.hide();
-		$contactList.mouseenter(function(){ $scontacts.fadeIn(); });
-		$contactList.mouseleave(function(){ $scontacts.fadeOut(); });
-	});
+    $scontacts.hide();
+    $contactList.mouseenter(function(){ $scontacts.fadeIn(); });
+    $contactList.mouseleave(function(){ $scontacts.fadeOut(); });
+
+    /**
+     * Tags & categories tab activation based on hash value. If hash is undefined then first tab is activated.
+     */
+    function activateTab() {
+      console.log('hashchange');
+      if(['/tags.html', '/categories.html'].indexOf(window.location.pathname) > -1) {
+        var hash = window.location.hash;
+        if(hash)
+          console.log(hash + ' aasdasd');
+        if(hash)
+          $('.tab-pane').length && $('a[href="' + hash + '"]').tab('show');
+        else
+          $('.tab-pane').length && $($('.cat-tag-menu li a')[0]).tab('show');
+      }
+    }
+
+    // watch hash change and activate relevant tab
+    $(window).on('hashchange', activateTab);
+
+    // initial activation
+    activateTab();
+  };
+
+  // run init on document ready
+  $(document).ready(init);
+
 })(jQuery);
